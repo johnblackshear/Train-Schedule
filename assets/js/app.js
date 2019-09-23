8b77430a58d04f0211b70a1f96fa084706def9e1
John Blackshear <swjaybee@gmail.com>
2019-09-22 19:22:45 -0500

    jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){//I need jquery to run!
 
 	console.log("testing...");
+	var config ={
+		apiKey = "AIzaSyBv57BMT1hFCKmHUgNRW46VTxHNuEVRxUA",
+		authDomain = "blacksher-trian-schedule.firebaseapp.com",
+		databaseURL = "https://blacksher-trian-schedule.firebaseio.com/",
+		projectId = "blacksher-trian-schedule",
+		storageBucket = "blacksher-trian-schedule.appspot.com",
+		messagingSenderId = "738813261960"
+	};
+	firebase.initializeApp(config);
 
+	var database = firebase.database();
+
+	var name;
+	var destination;
+	var firstTrain;
+	var frequency;
+
+	$("#add-user").on("click", function(){
+		event.preventDefault();
+
+	})
 
 
 });
